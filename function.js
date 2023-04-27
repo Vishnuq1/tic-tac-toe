848e227f3d720f5d484c00af5b55a1d3feb8b99a
vishnu <vishnubhel@rediffmail.com>
2023-04-28 00:37:47 +0530

    commit

---
+++ function.js
@@ -0,0 +1,17 @@
+
+
+     function fruit(){
+        console.log(price);
+         console.log(name);
+
+         let price= 20;
+         let name= "apple";
+     }
+     fruit()
+   //  console.log("ho")
+
+// function hi(){
+//     console.log("vishnu")
+
+// }
+// hi()
